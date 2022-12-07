977367a46e9a5a1b88de3eec296e66345279264e
Ubaid <mrubaid56@gmail.com>
2022-12-07 23:48:41 +0530

    Password Generator

---
+++ password_generator/main.py
@@ -0,0 +1,20 @@
+import random 
+
+print("Welcome to Your Password Generator ")
+
+chars = "abcdefghijklmnoporstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*().,?0123456789"
+
+number = input("Amount of passwords to generate: ")
+number = int(number)
+
+length = input("Your password length: ")
+length = int(length)
+
+print("\nHere are your passwords: ")
+
+for pwd in range(number): 
+    passwords = ""
+    for c in range(length):
+        passwords += random.choice(chars)
+    print(passwords)
+
