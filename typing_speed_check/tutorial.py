cffd4299dfb40333d49a60f3b3cfc11f165ef03d
Ubaid <mrubaid56@gmail.com>
2022-12-08 22:40:53 +0530

    styling the terminal

--- typing_speed_check/tutorial.py
+++ typing_speed_check/tutorial.py
@@ -2,8 +2,11 @@ import curses
 from curses import wrapper
 
 def main(stdscr):
+    curses.init_pair(1,curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2,curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3,curses.COLOR_WHITE, curses.COLOR_BLACK)
     stdscr.clear()
-    stdscr.addstr("Hello world")
+    stdscr.addstr(0, 0, "Hello world")
     stdscr.refresh()
     stdscr.getkey()
 
